24bdbdf663c76d7b128378fe7c40fae87c6e35d3
Guilherme Gandolfi <guilherme_gandolfi@hotmail.com>
2020-05-18 21:37:05 -0300

    class of connection with database

---
+++ conexaoBanco.py
@@ -0,0 +1,12 @@
+'''
+
+    Autor: Guilherme Gandolfi
+
+    Data: 18/05/2020
+
+    Desc: The class is a connection with database and other functions
+
+'''
+
+class conexaoBanco:
+    print('open connetion')
